6b60d6438d984e2f9851624f9b9d9e9855de13bc
ulassatr <ulassatr@gmail.com>
2017-11-23 15:00:00 +0300

    SearchEngine v0.1

---
+++ SearchEngine/Search.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="SearchEngine.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 81px">
+
+            <asp:TextBox ID="UrlText" runat="server" Width="194px"></asp:TextBox>
+&nbsp;URL<br />
+            <br />
+            <asp:TextBox ID="KeyText" runat="server" Width="193px"></asp:TextBox>
+&nbsp;Anahtar Kelime<br />
+            <br />
+            <asp:Button ID="SearchButton" runat="server" Text="Ara" OnClick="SearchButton_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ SearchEngine/Search.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using HtmlAgilityPack;
+namespace SearchEngine
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SearchButton_Click(object sender, EventArgs e)
+        {
+            //  string adresal = UrlText.Text;
+            //  WebResponse GelenCevap;
+            //  WebRequest adresistegi = HttpWebRequest.Create(adresal);
+            //  GelenCevap = adresistegi.GetResponse();
+            //// CultureInfo tr = new CultureInfo("tr - TR");
+            //  StreamReader SayfaKaynakBilgisi = new StreamReader(GelenCevap.GetResponseStream());
+            //  string kaynakbilgiyial = SayfaKaynakBilgisi.ReadToEnd();
+
+            //   string url = UrlText.Text;
+            //    WebRequest req = HttpWebRequest.Create(url);
+            //    WebResponse res;
+            //    try
+            //    {
+
+            //        res = req.GetResponse();
+
+            //        StreamReader data = new StreamReader(res.GetResponseStream(), System.Text.Encoding.GetEncoding("windows-1254"));
+            //        string icerik = data.ReadToEnd();
+
+            //        //int start = icerik.IndexOf("<h1>") + 4;
+            //        //int end = icerik.IndexOf("</h1>");
+            //        //string h1 = icerik.Substring(start, end - start);
+            //        //richTextBox1.Text = h1;
+            //    }
+            //    catch
+            //    { /*richTextBox1.Text = "Sayfa okunamadı!";*/ }
+
+
+            //}
+            //string url = UrlText.Text;
+            //var html = new HtmlDocument();
+            //html.LoadHtml(new WebClient().DownloadString(url));
+            //var root = html.DocumentNode;
+            //var nodes = root.Descendants();
+            //var totalNodes = nodes.Count();
+            string url = UrlText.Text;
+            string document = GetSourceCode(url);
+            KeyText.Text = document;
+        }
+        static string GetSourceCode(string url)
+        {
+            // web isteği oluştur
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+            // gelen cevabı al
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            // cevapla gelen veriyi oku
+            using (StreamReader sRead = new StreamReader(resp.GetResponseStream(), Encoding.UTF8))
+            {
+                // veriyi döndür
+                return sRead.ReadToEnd();
+            }
+        }
+    }
+}
