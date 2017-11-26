98ad630118f82150515e2f6a3b85262176676ca9
ulassatr <ulassatr@gmail.com>
2017-11-26 22:11:48 +0300

    SearchEngine

--- SearchEngine/Search.aspx.cs
+++ SearchEngine/Search.aspx.cs
@@ -20,59 +20,38 @@ namespace SearchEngine
 
         protected void SearchButton_Click(object sender, EventArgs e)
         {
-            //  string adresal = UrlText.Text;
-            //  WebResponse GelenCevap;
-            //  WebRequest adresistegi = HttpWebRequest.Create(adresal);
-            //  GelenCevap = adresistegi.GetResponse();
-            //// CultureInfo tr = new CultureInfo("tr - TR");
-            //  StreamReader SayfaKaynakBilgisi = new StreamReader(GelenCevap.GetResponseStream());
-            //  string kaynakbilgiyial = SayfaKaynakBilgisi.ReadToEnd();
-
-            //   string url = UrlText.Text;
-            //    WebRequest req = HttpWebRequest.Create(url);
-            //    WebResponse res;
-            //    try
-            //    {
-
-            //        res = req.GetResponse();
-
-            //        StreamReader data = new StreamReader(res.GetResponseStream(), System.Text.Encoding.GetEncoding("windows-1254"));
-            //        string icerik = data.ReadToEnd();
-
-            //        //int start = icerik.IndexOf("<h1>") + 4;
-            //        //int end = icerik.IndexOf("</h1>");
-            //        //string h1 = icerik.Substring(start, end - start);
-            //        //richTextBox1.Text = h1;
-            //    }
-            //    catch
-            //    { /*richTextBox1.Text = "Sayfa okunamadı!";*/ }
-
-
-            //}
-            //string url = UrlText.Text;
-            //var html = new HtmlDocument();
-            //html.LoadHtml(new WebClient().DownloadString(url));
-            //var root = html.DocumentNode;
-            //var nodes = root.Descendants();
-            //var totalNodes = nodes.Count();
             string url = UrlText.Text;
-            string document = GetSourceCode(url);
-            KeyText.Text = document;
-        }
-        static string GetSourceCode(string url)
-        {
-            // web isteği oluştur
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            // gelen cevabı al
-            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            var client = new WebClient { Encoding = System.Text.Encoding.UTF8 };
 
-            // cevapla gelen veriyi oku
-            using (StreamReader sRead = new StreamReader(resp.GetResponseStream(), Encoding.UTF8))
+            String htmlz = client.DownloadString(url);
+
+            String html = HttpUtility.HtmlDecode(htmlz).ToString();
+
+            HtmlAgilityPack.HtmlDocument dokuman = new HtmlAgilityPack.HtmlDocument();
+            dokuman.LoadHtml(html);
+            string htmlSon = dokuman.ToString();
+
+            //HtmlNodeCollection basliklar = dokuman.DocumentNode.SelectNodes("//li[@class='post-listing-list-item__post post-listing-list-item__post--featured']");
+
+            int sayac = 0;
+            
+            string metin = html;
+            
+            string kelime = KeyText.Text;
+            
+            int konum = metin.IndexOf(kelime);
+            
+            while (konum != -1)
             {
-                // veriyi döndür
-                return sRead.ReadToEnd();
+                konum = metin.IndexOf(kelime, konum + 1);
+
+                sayac++;
+                
             }
+
         }
     }
 }
