9d27331471f01e90e1413346a3994d453db2e289
ygtegeyvz <ygtegeyvz@gmail.com>
2017-12-05 10:29:14 +0300

    SearchEngine v.0.8.6

---
+++ SearchEngine/SiteSiralama.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SiteSiralama.aspx.cs" Inherits="SearchEngine.SiteSiralama" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body style="height: 354px">
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" Width="594px"></asp:TextBox>
+            <br />
+            <br />
+        </div>
+        <asp:TextBox ID="TextBox2" runat="server" Width="592px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" Width="102px" />
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox3" runat="server"  ReadOnly="True" TextMode="MultiLine" Width="586px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ SearchEngine/SiteSiralama.aspx.cs
@@ -0,0 +1,130 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SearchEngine
+{
+    public partial class SiteSiralama : System.Web.UI.Page
+    {
+        public List<string> url_list = new List<string>();
+        public List<string> kelime_list = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            linkleriBul();
+        }
+        public void linkleriBul()
+        {
+            //Kullanıcının girdiği urlleri listeye attık
+            string[] url_kumesi = TextBox1.Text.Split(',');
+
+            for (int i = 0; i < url_kumesi.Count(); i++)
+            {
+                url_list.Add(url_kumesi[i]);
+
+            }
+            //Kullanıcının girdiği kelimeleri listeye attık
+            string[] kelime_kumesi = TextBox2.Text.Split(',');
+
+            for (int i = 0; i < kelime_kumesi.Count(); i++)
+            {
+                kelime_list.Add(kelime_kumesi[i]);
+
+            }
+
+            string htmlstring;
+            List<string> linkListesi = new List<string>();
+            List<string> yuhAqIkıncıDerinlik = new List<string>();
+            Htmlİslemleri Cek_veri = new Htmlİslemleri();
+            List<string> liste = new List<string>();
+            //Url listesi kadar htmllerini çekiyoruz
+            for (int j = 0; j < url_list.Count; j++)
+            {
+                htmlstring = Cek_veri.GetVeri(url_list[j]);
+                HtmlAgilityPack.HtmlDocument htmldoc = new HtmlAgilityPack.HtmlDocument();
+                htmldoc.LoadHtml(htmlstring);
+
+                List<string> hrefTags = new List<string>();
+                foreach (HtmlNode link in htmldoc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    HtmlAttribute att = link.Attributes["href"];
+                    hrefTags.Add(att.Value); //linklerimizi alıyoruz
+                }
+                liste.AddRange(altDerinligeIn(hrefTags, j));
+            }
+       //     yuhAqIkıncıDerinlik.AddRange(ikinciDerinlikGeliyoBaba(liste));
+            //Öylesine yazdım
+        }
+
+        public List<string> altDerinligeIn(List<string> hrefTags, int k)
+        {
+            List<string> dogruLinkler = new List<string>();
+            List<bool> deneme = new List<bool>();
+            for (int i = 0; i < hrefTags.Count; i++)
+            {
+                deneme.Add(hrefTags[i].StartsWith(url_list[k]));
+            }
+
+            for (int i = 0; i < deneme.Count; i++)
+            {
+                if (deneme[i] == true)
+                {
+                    dogruLinkler.Add(hrefTags[i]);
+
+                }
+            }
+            return dogruLinkler;
+        }
+        /*
+        public List<string> ikinciDerinlikGeliyoBaba(List<string> linkler)
+        {
+            List<string> linkListesi = new List<string>();
+            List<bool> deneme = new List<bool>();
+            string htmlstring;
+            Htmlİslemleri Cek_veri = new Htmlİslemleri();
+            for (int i = 0; i < linkler.Count; i++)
+            {
+
+                htmlstring = Cek_veri.GetVeri(linkler[i]);
+                HtmlAgilityPack.HtmlDocument htmldoc = new HtmlAgilityPack.HtmlDocument();
+                htmldoc.LoadHtml(htmlstring);
+
+                List<string> hrefTags = new List<string>();
+                if (htmldoc.DocumentNode.SelectNodes("//a[@href]") != null)
+                {
+                    foreach (HtmlNode link in htmldoc.DocumentNode.SelectNodes("//a[@href]"))
+                    {
+                        HtmlAttribute att = link.Attributes["href"];
+                        hrefTags.Add(att.Value);
+                    }
+                    for (int b = 0; b < hrefTags.Count; b++)
+                    {
+                        deneme.Add(hrefTags[b].StartsWith(linkler[i]));
+                    }
+                    for (int j = 0; j < deneme.Count; j++)
+                    {
+                        if (deneme[j] == true)
+                        {
+                            linkListesi.Add(hrefTags[j]);
+
+                        }
+                    }
+                }
+                else continue;
+              
+
+              
+            }
+            return linkListesi;
+        }
+        */
+    }
+}
